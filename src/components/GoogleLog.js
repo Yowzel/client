40317339b730ee2aead5d4a41379014eae6facf1
Gia-Huy N Gonzalez <39535243+Yowzel@users.noreply.github.com>
2023-05-11 20:21:34 -0700

    removing gogole stuff

--- src/components/GoogleLog.js
+++ src/components/GoogleLog.js
@@ -1,20 +1,7 @@
 
 
-const clientId = "553859060156-h3632svbkihq01qs1p68uakep7glhs2a.apps.googleusercontent.com"
-
 function Login() {
-
-    const onSuccess = (res) => {
-        console.log("LOGIN SUCCESS! Current user: ", res.profileObj);
-    }
-
-    const onFailure = (res) => {
-        console.log("Login failed! res : " ,res);
-    }
-
-    return <div id='signInButton'>
-        <div
-        />
+    <div>
     </div>
 }
 
